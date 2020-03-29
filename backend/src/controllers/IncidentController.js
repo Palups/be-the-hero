4f6c71ff79f2c91aee545c9c3877a878bc09bd3f
Vinícius Palu <vinicius.palu@live.com>
2020-03-29 03:58:30 -0300

    Validações e execução de testes

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response){
-        const {page} = request.query;
+        const {page = 1} = request.query;
         
         const [count] = await connection('incidents').count();
         response.header('X-Total-Count', count['count(*)']);
